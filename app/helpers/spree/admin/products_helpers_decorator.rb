e2012ba17c30b542d3385c2f52ef7389ea1a50d5
Alexis Ferrada <alexis@acid.cl>
2018-06-06 16:04:48 -0400

    Fix modules in spree

--- app/helpers/spree/admin/products_helpers_decorator.rb
+++ app/helpers/spree/admin/products_helpers_decorator.rb
@@ -1,5 +1,9 @@
-Spree::Admin::ProductsHelper.class_eval do
-  def display_featured_icon(product)
-    content_tag(:i, '', class: "glyphicon glyphicon-#{ product.featured ? 'ok' : 'remove'}")
-  end
+module Spree
+	module Admin
+		module ProductsHelper
+		  def display_featured_icon(product)
+		    content_tag(:i, '', class: "glyphicon glyphicon-#{ product.featured ? 'ok' : 'remove'}")
+		  end
+		end
+	end
 end
