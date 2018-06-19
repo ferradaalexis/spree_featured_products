a20fa9dbb716c8acfd813c6a57b7eb886bf2fa15
Alexis Ferrada <alexis@acid.cl>
2018-06-19 17:04:16 -0400

    Fix Removed Products Helper from spree

--- app/helpers/spree/admin/products_helpers_decorator.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-	module Admin
-		module ProductsHelper
-		  def display_featured_icon(product)
-		    content_tag(:i, '', class: "glyphicon glyphicon-#{ product.featured ? 'ok' : 'remove'}")
-		  end
-		end
-	end
-end
